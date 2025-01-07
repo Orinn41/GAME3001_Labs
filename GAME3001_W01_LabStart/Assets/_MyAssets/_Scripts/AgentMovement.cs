bdee3f8e8b7c0e1d4b1111aaff2cb8c5bf896f5b
Orinn41 <boranakkoyun41@gmail.com>
2025-01-07 11:44:03 -0500

    When Agent Too close to the Island Game Finishes

--- GAME3001_W01_LabStart/Assets/_MyAssets/_Scripts/AgentMovement.cs
+++ GAME3001_W01_LabStart/Assets/_MyAssets/_Scripts/AgentMovement.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AgentMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private Transform Island;
+    [SerializeField]
+    private float endSceneIslandDistance;
+
     private Vector3 targetPosition = Vector3.zero;
     // Update is called once per frame
     void Update()
@@ -21,6 +27,11 @@ public class AgentMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         // rotate to look at target location 
         LookAt2D(targetPosition);
+
+        if (Vector3.Distance(transform.position, Island.position) <= endSceneIslandDistance)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
     void LookAt2D(Vector3 target)
     { 
