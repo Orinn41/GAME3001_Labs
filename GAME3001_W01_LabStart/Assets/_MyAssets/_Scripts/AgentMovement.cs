fb821a66c0465767933d379cc62202e1301e4341
Orinn41 <boranakkoyun41@gmail.com>
2025-01-07 11:26:12 -0500

    Agent Movement Basic Move To Target Feature Created

---
+++ GAME3001_W01_LabStart/Assets/_MyAssets/_Scripts/AgentMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    private Vector3 targetPosition = Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
+        // checking mouse clicks 
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse has been clicked");
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // convert mouse position to world position
+            targetPosition.z = 0f; // ensure the Z-cordinate is correct for 2D game 
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
