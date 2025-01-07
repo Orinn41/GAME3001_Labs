27212e01965a14b2db9464271588de3dc4be86f3
Orinn41 <boranakkoyun41@gmail.com>
2025-01-07 11:32:47 -0500

    Agent Rotate To Movement Direction

--- GAME3001_W01_LabStart/Assets/_MyAssets/_Scripts/AgentMovement.cs
+++ GAME3001_W01_LabStart/Assets/_MyAssets/_Scripts/AgentMovement.cs
@@ -17,7 +17,15 @@ public class AgentMovement : MonoBehaviour
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // convert mouse position to world position
             targetPosition.z = 0f; // ensure the Z-cordinate is correct for 2D game 
         }
-
+        // move towards to target position
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        // rotate to look at target location 
+        LookAt2D(targetPosition);
+    }
+    void LookAt2D(Vector3 target)
+    { 
+       Vector3 lookDirection = target - transform.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
