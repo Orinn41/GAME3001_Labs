f5bb654138c449d7e4ac284e53a8fafe3136cf99
Orinn41 <boranakkoyun41@gmail.com>
2025-02-02 18:05:34 -0500

    Fixed all issues except random range Assignment 1 is ready

--- Assingment1/Assets/Scripts/GameController.cs
+++ Assingment1/Assets/Scripts/GameController.cs
@@ -2,28 +2,69 @@ using System;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private GameObject object0;
+    [SerializeField] private GameObject object1;
+    [SerializeField] private GameObject object2;
+    [SerializeField] private GameObject object3;
     public GameObject characterPrefab;
     public GameObject targetPrefab;
     public GameObject enemyPrefab;
     public GameObject obstaclePrefab;
 
-    private GameObject characterInstance;
-    private GameObject targetInstance;
-    private GameObject enemyInstance;
-    private GameObject obstacleInstance;
+    private List<GameObject> instantiatedObjects = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SetAllObjectsInactive();
         characterPrefab.SetActive(false);
         targetPrefab.SetActive(false);  
         enemyPrefab.SetActive(false);
         obstaclePrefab.SetActive(false);
     }
-
-    // Update is called once per frame
+    public void SetAllObjectsInactive()
+    {
+        object0.SetActive(false);
+        object1.SetActive(false);
+        object2.SetActive(false);
+        object3.SetActive(false);
+    }
+    public void SetObject0Active()
+    {
+        object0.SetActive(true);
+    }
+    public void setObject1Active()
+    {
+        object1.SetActive(true);
+    }
+    public void setObject2Active()
+    {
+        object2.SetActive(true);
+    }
+    public void setObject3Active()
+    {
+        object3.SetActive(true);
+    }
+    public void MoveObjectsRandom()
+    {
+        object0.transform.position = GetRandomPosition();
+        object1.transform.position = GetRandomPosition();
+        object2.transform.position = GetRandomPosition();
+        object3.transform.position = GetRandomPosition();
+    }
+    private Vector3 GetRandomPosition()
+    {
+        float x = UnityEngine.Random.Range(-5f, 5f);
+        float y = UnityEngine.Random.Range(-5f, 5f);
+        float z = 0;
+        return new Vector3(x, y, z);
+    }
+    // Update i
+    // s called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) StartSeeking();
@@ -34,46 +75,57 @@ public class GameController : MonoBehaviour
     }
     void StartSeeking()
     {
-        characterInstance = Instantiate(characterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        targetInstance = Instantiate(targetPrefab, new Vector3(5,0, 0), Quaternion.identity);
+        GameObject characterInstance = Instantiate(characterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject targetInstance = Instantiate(targetPrefab, new Vector3(5,0, 0), Quaternion.identity);
         characterInstance.SetActive(true);
         targetInstance.SetActive(true);
         characterInstance.GetComponent<SeekingBehavior>().target = targetInstance.transform;
+        instantiatedObjects.Add(characterInstance);
+        instantiatedObjects.Add(targetInstance);
     }
     void StartFleeing()
     {
-        characterInstance = Instantiate(characterPrefab, new Vector3(0,0,0), Quaternion.identity);
-        enemyInstance = Instantiate(enemyPrefab, new Vector3(5, 0, 0),Quaternion.identity);
+        GameObject characterInstance = Instantiate(characterPrefab, new Vector3(0,0,0), Quaternion.identity);
+        GameObject enemyInstance = Instantiate(enemyPrefab, new Vector3(5, 0, 0),Quaternion.identity);
         characterInstance.SetActive(true);
         enemyInstance.SetActive(true);
         characterInstance.GetComponent<FleeingBehavior>().enemy = enemyInstance.transform;
+        instantiatedObjects.Add(characterInstance);
+        instantiatedObjects.Add(enemyInstance);
 
     }
     void StartArrival()
     {
-        characterInstance = Instantiate(characterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        targetInstance = Instantiate(targetPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+        GameObject characterInstance = Instantiate(characterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject targetInstance = Instantiate(targetPrefab, new Vector3(5, 0, 0), Quaternion.identity);
         characterInstance.SetActive(true);
         targetInstance.SetActive(true);
         characterInstance.GetComponent<ArrivalBehavior>().target = targetInstance.transform;
+        instantiatedObjects.Add(characterInstance);
+        instantiatedObjects.Add(targetInstance);
     }
     void StartAvoidance()
     {
-        characterInstance = Instantiate(characterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        targetInstance = Instantiate(targetPrefab, new Vector3(5, 0, 0), Quaternion.identity);
-        enemyInstance = Instantiate(enemyPrefab, new Vector3(2, 0, 0), Quaternion.identity);
+        GameObject characterInstance = Instantiate(characterPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject  targetInstance = Instantiate(targetPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+        GameObject enemyInstance = Instantiate(enemyPrefab, new Vector3(2, 0, 0), Quaternion.identity);
         characterInstance.SetActive(true);
         enemyInstance.SetActive(true);
         targetInstance.SetActive(true);
         characterInstance.GetComponent<AvoidanceBehavior>().target = targetInstance.transform;
         characterInstance.GetComponent<AvoidanceBehavior>().enemy = enemyInstance.transform;
+        instantiatedObjects.Add(characterInstance);
+        instantiatedObjects.Add(targetInstance);
+        instantiatedObjects.Add(enemyInstance);
 
     }
     void ResetScene()
     {
-        Destroy(characterInstance);
-        Destroy(targetInstance);
-        Destroy(enemyInstance);
-        Destroy(obstacleInstance);
+      foreach(GameObject obj  in instantiatedObjects)
+        {
+            if ( obj != null)
+              Destroy(obj);
+        }
+      instantiatedObjects.Clear();
     }
 }
