70d61e23baebf10c133e6e72b9a4d60d7f3834c3
Orinn41 <boranakkoyun41@gmail.com>
2025-02-11 10:36:05 -0500

    TileScript updated with setStatus function

--- Lab4/Assets/_MyAssets/_Scripts/TileScript.cs
+++ Lab4/Assets/_MyAssets/_Scripts/TileScript.cs
@@ -22,17 +22,33 @@ public class TileScript : MonoBehaviour
         gameObject.GetComponent<SpriteRenderer>().color = color;
     }
 
-    public void ToggleImpassable(bool impass = true)
+    public void SetStatus(TileStatus stat)
     {
-        if (impass)
+        status = stat;
+       switch (stat)
         {
-            status = TileStatus.IMPASSABLE;
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(0.5f, 0f, 0f, 0.5f);
-        }
-        else
-        {
-            status = TileStatus.UNVISITED;
-            gameObject.GetComponent<SpriteRenderer>().color = original;
+            case TileStatus.UNVISITED:
+                gameObject.GetComponent<SpriteRenderer>().color = original;
+                tilePanel.statusText.text = "U";
+                break;
+            case TileStatus.OPEN:
+                tilePanel.statusText.text = "O";
+                break;
+            case TileStatus.CLOSED:
+                tilePanel.statusText.text = "c";
+                break;
+            case TileStatus.IMPASSABLE:
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(.5f, 0f, 0f, .5f);
+                tilePanel.statusText.text = "I";
+                break;
+            case TileStatus.GOAL:
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0f, 0.5f);
+                tilePanel.statusText.text = "G";
+                break;
+            case TileStatus.START:
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(0.0f, 0.5f, 0.0f, 0.5f);
+                tilePanel.statusText.text = "S";
+                break;
         }
     }
 }
