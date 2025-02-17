d629b1cbe612fc6665d918e4a125c736de3014a8
Orinn41 <boranakkoyun41@gmail.com>
2025-02-16 20:36:16 -0500

    tilePanel and tilePanelScript are created canvas is setted up

---
+++ Lab4(2)/Assets/_MyAssets/_Scripts/TilePanelScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TilePanelScript : MonoBehaviour
+{
+    public TMP_Text statusText;
+    public TMP_Text costText;
+}
