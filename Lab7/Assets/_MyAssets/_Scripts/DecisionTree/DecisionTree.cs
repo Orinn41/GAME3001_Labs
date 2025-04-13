c5722dd3fd877310029ed92e544fcdd30545a229
Orinn41 <boranakkoyun41@gmail.com>
2025-04-13 18:15:49 -0400

    Decision tree now can add node and make decision

--- Lab7/Assets/_MyAssets/_Scripts/DecisionTree/DecisionTree.cs
+++ Lab7/Assets/_MyAssets/_Scripts/DecisionTree/DecisionTree.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 // TODO: Fill in for Lab 7a.
 public class DecisionTree
 {
+    public GameObject Agent { get; set; }
+    public LOSCondition LOSNode { get; set; }
+    public RadiusCondition RadiusNode { get; set; }
+    public CloseCombatCondition CloseCombatNode { get; set; }
+
+    public List<TreeNode> treeNodeList;
+
     // TODO: Fill in properties.
 
     public DecisionTree(GameObject agent)
     {
         // TODO: Fill in for Lab 7a.
+        Agent = agent;
+        treeNodeList = new List<TreeNode>();
     }
 
     public void MakeDecision()
     {
         // TODO: Fill in for Lab 7a.
+        TreeNode currentNode = treeNodeList[0];
+        while (!currentNode.isLeaf)
+        {
+            currentNode = ((ConditionNode)currentNode).Condition() ? currentNode.right : currentNode.left;
+        }
+        ((ActionNode)currentNode).Action();
     }
 
     public TreeNode AddNode(TreeNode parent, TreeNode child, TreeNodeType type)
     {
         // TODO: Fill in for Lab 7a.
+        switch (type)
+        {
+            case TreeNodeType.LEFT_TREE_NODE:
+                parent.left = child;
+                break;
+
+            case TreeNodeType.RIGHT_TREE_NODE:
+                parent.right = child;
+                break;
+        }
+        child.parent = parent;
+        return child;  
     }
 }
