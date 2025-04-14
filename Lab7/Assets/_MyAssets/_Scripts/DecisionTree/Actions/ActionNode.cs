ddae8a6c70fefe0ef175f5b2bd2b1def990585ff
Orinn41 <boranakkoyun41@gmail.com>
2025-04-13 21:46:06 -0400

    Agent object and action node updated

--- Lab7/Assets/_MyAssets/_Scripts/DecisionTree/Actions/ActionNode.cs
+++ Lab7/Assets/_MyAssets/_Scripts/DecisionTree/Actions/ActionNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,18 @@ using UnityEngine;
 public abstract class ActionNode : TreeNode
 {
     public GameObject Agent { get; set; }
+    public Component AgentScript { get; set; }
 
     public ActionNode()
     {
         isLeaf = true;
         Agent = null;
+        AgentScript = null;
+    }
+    public void SetAgent(GameObject agent, Type script)
+    {
+        Agent = agent;  
+        AgentScript = agent.GetComponent(script);
     }
 
     public abstract void Action(); // Abstract method for tree.
